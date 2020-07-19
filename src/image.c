c9c2254571b6e410a325ce3ef1661a7cded7563e
dfranx <dfranx00@gmail.com>
2020-07-19 15:07:45 +0200

    Small change to how spvm_image_sample works

--- src/image.c
+++ src/image.c
@@ -2,6 +2,7 @@
 #include <spvm/value.h>
 #include <string.h>
 #include <stdlib.h>
+#include <math.h>
 
 void spvm_image_create(spvm_image_t img, float* data, int width, int height, int depth)
 {
@@ -14,9 +15,9 @@ void spvm_image_create(spvm_image_t img, float* data, int width, int height, int
 }
 float* spvm_image_sample(spvm_image_t img, float s, float t, float u)
 {
-	int x = (img->width - 1) * CLAMP(s, 0.0f, 1.0f);
-	int y = (img->height - 1) * CLAMP(t, 0.0f, 1.0f);
-	int z = (img->depth - 1) * CLAMP(u, 0.0f, 1.0f);
+	int x = roundf((img->width - 1) * CLAMP(s, 0.0f, 1.0f)); // nearest neighbor (?)
+	int y = roundf((img->height - 1) * CLAMP(t, 0.0f, 1.0f));
+	int z = roundf((img->depth - 1) * CLAMP(u, 0.0f, 1.0f));
 
 	return img->data + (z * img->height * img->width + y * img->width + x) * 4;
 }
