26772736b3747f5b690f78e3e855d80f04775fe1
mtehver <mark.tehver@gmail.com>
2022-04-18 22:29:24 +0300

    Add C++ header guard to extension header

--- inc/spvm/ext/GLSL450.h
+++ inc/spvm/ext/GLSL450.h
@@ -1,8 +1,16 @@
 #ifndef __SPRIV_VM_EXT_GLSL_450_H__
 #define __SPRIV_VM_EXT_GLSL_450_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
 #include <spvm/opcode.h>
 
 spvm_ext_opcode_func* spvm_build_glsl450_ext();
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
 
 #endif // __SPRIV_VM_EXT_GLSL_450_H__
